488499bd2e1639a9ffcf3ffd303ef00d0724976b
mireole <mireole@proton.me>
2025-07-23 14:15:39 +0200

    Update dependencies

--- scripts/update_deps.sh
+++ scripts/update_deps.sh
@@ -5,8 +5,8 @@ set -e
 scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
 # limine
-zig fetch --save=limine git+https://github.com/limine-bootloader/limnine#v9.x-binary
-zig fetch --save git+https://github.com/uACPI/zuacpi?ref=mai
+zig fetch --save=limine git+https://github.com/limine-bootloader/limine#v9.x-binary
+zig fetch --save git+https://github.com/uACPI/zuacpi?ref=main
 
 # ovmf
 "$scriptDir/fetch_ovmf.sh"
